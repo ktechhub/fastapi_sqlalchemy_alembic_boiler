7776fe244ed67678c8af0c3bfb8a57049593e474
Mumuni Mohammed <mumunim10@gmail.com>
2025-11-15 08:40:15 +0100

    feat: added sessions

--- app/database/base_class.py
+++ app/database/base_class.py
@@ -1,6 +1,7 @@
 # app/database/base_class.py
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy.orm import DeclarativeMeta
+from sqlalchemy import MetaData
 
 
 class CustomBase:
@@ -10,4 +11,13 @@ class CustomBase:
         return cls.__name__.lower()
 
 
-Base: DeclarativeMeta = declarative_base(cls=CustomBase)
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata_obj = MetaData(naming_convention=convention)
+
+Base: DeclarativeMeta = declarative_base(cls=CustomBase, metadata=metadata_obj)
